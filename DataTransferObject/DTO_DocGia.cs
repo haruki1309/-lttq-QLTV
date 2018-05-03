388f4d0e7ce44f6b573668f2bd836271c6ce5bdd
lgtent <34021692+lgtent@users.noreply.github.com>
2018-05-03 10:05:27 +0700

    Add files via upload

---
+++ DataTransferObject/DTO_DocGia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject
+{
+    class DTO_DocGia
+    {
+        private string maDocGia;
+        private string hoTen;
+        private string diaChi;
+        private string cmnd;
+        private string ngaySinh;
+        private string ngayDK;
+    }
+}
