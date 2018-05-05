20019103c261facf2b0ec4fa736bed024734d070
lgtent <34021692+lgtent@users.noreply.github.com>
2018-05-05 19:48:03 +0700

    Add files via upload

--- DataTransferObject/DTO_DocGia.cs
+++ DataTransferObject/DTO_DocGia.cs
@@ -11,16 +11,29 @@ namespace DataTransferObject
         private string maDocGia;
         private string hoTen;
         private string diaChi;
+        private string soDT;
         private string cmnd;
-        private string ngaySinh;
-        private string ngayDK;
+        private DateTime ngaySinh;
+        private DateTime ngayDK;
 
         
         public string HoTen { get => hoTen; set => hoTen = value; }
         public string DiaChi { get => diaChi; set => diaChi = value; }
         public string Cmnd { get => cmnd; set => cmnd = value; }
-        public string NgaySinh { get => ngaySinh; set => ngaySinh = value; }
-        public string NgayDK { get => ngayDK; set => ngayDK = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public DateTime NgayDK { get => ngayDK; set => ngayDK = value; }
         public string MaDocGia { get => maDocGia; set => maDocGia = value; }
+        public string SoDT { get => soDT; set => soDT = value; }
+
+        public DTO_DocGia(string maDocGia, string hoTen, string diaChi, string soDT, string cmnd, DateTime ngaySinh, DateTime ngayDK)
+        {
+            this.maDocGia = maDocGia;
+            this.hoTen = hoTen;
+            this.diaChi = diaChi;
+            this.soDT = soDT;
+            this.cmnd = cmnd;
+            this.ngaySinh = ngaySinh;
+            this.ngayDK = ngayDK;
+        }             
     }
 }
---
+++ DatabaseAccessLayer/DAL_DocGia.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTransferObject;
+
+namespace DatabaseAccessLayer
+{
+    public class DAL_DocGia: DBConnect
+    {
+        public DataTable Get()
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from DocGia", cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public DataTable Get(string Condition)
+        {
+            try
+            {
+                string SQL = String.Format("select * from DocGia where {0}", Condition);
+                SqlDataAdapter da = new SqlDataAdapter(SQL, cn);
+                
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+        }
+        DataTable dt = new DataTable();
+        //Them
+        public bool Insert(DTO_DocGia dTO_DocGia)
+        {
+            try
+            {
+                cn.Open();
+                string SQL = string.Format("INSERT INTO ThuThu VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')", dTO_DocGia.MaDocGia, dTO_DocGia.HoTen, dTO_DocGia.DiaChi, dTO_DocGia.SoDT, dTO_DocGia.Cmnd, dTO_DocGia.NgaySinh, dTO_DocGia.NgayDK);
+                SqlCommand cmd = new SqlCommand(SQL, cn);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return false;
+        }
+        //Sua
+        public bool Update(DTO_DocGia dTO_DocGia)
+        {
+            try
+            {
+                cn.Open();
+                string SQL = string.Format("UPDATE ThuThu SET HoTen = '{0}', DiaChi = '{1}', SDT = '{2}', CMND = '{3}', NgaySinh = '{4}', NgayDK = '{5}' WHERE MaDocGia = '{6}'", dTO_DocGia.HoTen, dTO_DocGia.DiaChi, dTO_DocGia.SoDT, dTO_DocGia.Cmnd, dTO_DocGia.NgaySinh, dTO_DocGia.NgayDK, dTO_DocGia.MaDocGia);
+                SqlCommand cmd = new SqlCommand(SQL, cn);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return false;
+        }
+        //Xoa
+        public bool Delete(string MaDocGia)
+        {
+            try
+            {
+                cn.Open();
+                string SQL = string.Format("DELETE FROM DocGia WHERE MaDocGia = {0}", MaDocGia);
+                SqlCommand cmd = new SqlCommand(SQL, cn);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return false;
+        }
+
+        // bug cho nay
+    //    public int CountRow()
+    //    {
+    //        try
+    //        {
+    //            cn.Open();
+                
+    //            SqlCommand cmd = cn.CreateCommand();
+    //            DbDataReader reader = cmd.ExecuteReader();
+    //            reader.getV
+    //            if (cmd.ExecuteNonQuery() > 0)
+    //            {
+    //                return true;
+    //            }
+    //        }
+    //        catch (Exception e)
+    //        {
+
+    //        }
+    //        finally
+    //        {
+    //            cn.Close();
+    //        }
+    //        return false;
+    //    }
+    }
+}
