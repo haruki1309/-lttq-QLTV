ca6c88c6077331b6253065ad7a5d3f4b250145c1
catu29 <38588214+catu29@users.noreply.github.com>
2018-05-06 00:57:25 +0700

    Update BUS_DocGia + Form chính (Sửa, Xóa độc giả)

---
+++ BUS_DocGia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTransferObject;
+using DatabaseAccessLayer;
+using System.Data;
+
+namespace BusinessLogicLayer
+{
+    public class BUS_DocGia
+    {
+        DAL_DocGia dalDocGia = new DAL_DocGia();
+
+        public DataTable getDocGia()
+        {
+            return dalDocGia.Get();
+        }
+        public DataTable getDocGia(string MaDocGia)
+        {
+            return dalDocGia.Get(MaDocGia);
+        }
+
+        //public bool insertDocGia(string prvMaDocGia, string hoTen, string diaChi, string soDT, string cmnd, DateTime ngaySinh, DateTime ngayDK)
+        //{
+        //    int iMaDocGia;
+        //    DataTable dt = dalDocGia.Get();
+        //    return dalDocGia.Insert(dTO_DocGia);
+        //}
+        public bool updateDocGia(DTO_DocGia dTO_DocGia)
+        {
+            return dalDocGia.Update(dTO_DocGia);
+        }
+        public bool deleteDocGia(string MaDocGia)
+        {
+            return dalDocGia.Delete(MaDocGia);
+        }        
+    }
+}
